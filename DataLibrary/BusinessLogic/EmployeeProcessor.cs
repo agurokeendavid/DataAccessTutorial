267647d2e2c092c6e4b9731ad9dcce9708135637
Keen David Aguro <agurokeendavid@gmail.com>
2020-03-02 11:29:33 +0800

    Finished

---
+++ DataLibrary/BusinessLogic/EmployeeProcessor.cs
@@ -0,0 +1,36 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.BusinessLogic
+{
+    public static class EmployeeProcessor
+    {
+        public static int CreateEmployee(int employeeId, string firstName, string lastName, string emailAddress)
+        {
+            var data = new EmployeeModel 
+            { 
+                EmployeeId = employeeId,
+                FirstName = firstName,
+                LastName = lastName,
+                EmailAddress = emailAddress
+            };
+
+            string sql = @"INSERT INTO TBL_DATAACCESS (EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL_ADDRESS) 
+                            VALUES (:EmployeeId, :FirstName, :LastName, :EmailAddress)";
+
+            return SqlDataAccess.SaveData(sql, data);
+        }
+
+        public static List<EmployeeModel> LoadEmployees()
+        {
+            string sql = @"SELECT ID Id, EMPLOYEE_ID EmployeeId, FIRST_NAME FirstName, LAST_NAME LastName, EMAIL_ADDRESS EmailAddress 
+                            FROM TBL_DATAACCESS";
+            return SqlDataAccess.LoadData<EmployeeModel>(sql);
+        }
+    }
+}
